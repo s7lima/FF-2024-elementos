541de7566949b5328f7fe3aa80cc354f08d78d81
s7lima <bruno.eslima@senacsp.edu.br>
2024-09-30 19:45:06 -0300

    Feat: Capturando elemento por id

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Selecionado div pelo id
+let div = document.getElementById('meuDiv');
+console.log(meuDiv);
+
+// Selecionando todos so parágrafos com a classe "parágrafo"
+let paragrafos = document.getElementsByClassName('paragrafo');
+
+// Selecionando o primeiro parágrafo com a classe "paragrafo"
+let primeiroParagrafo = document.querySelector('.paragrafo');
+
+// Selecionando todos os parágrafos dentro do div 
+let paragrafoDentroDiv = document.querySelectorAll('#meuDiv p');
