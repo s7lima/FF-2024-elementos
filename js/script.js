24a38b441c6db3849f18b70f260b5212ba266677
s7lima <bruno.eslima@senacsp.edu.br>
2024-09-30 19:51:51 -0300

    Feat: capturando elementos

--- js/script.js
+++ js/script.js
@@ -4,9 +4,12 @@ console.log(meuDiv);
 
 // Selecionando todos so parágrafos com a classe "parágrafo"
 let paragrafos = document.getElementsByClassName('paragrafo');
+console.log(paragrafos)
 
 // Selecionando o primeiro parágrafo com a classe "paragrafo"
 let primeiroParagrafo = document.querySelector('.paragrafo');
+console.log(primeiroParagrafo)
 
 // Selecionando todos os parágrafos dentro do div 
 let paragrafoDentroDiv = document.querySelectorAll('#meuDiv p');
+console.log(paragrafoDentroDiv)
