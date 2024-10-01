ddb5475ddd55405531277d0c02be9ccf4598729c
s7lima <bruno.eslima@senacsp.edu.br>
2024-09-30 21:27:03 -0300

    Feat: Ex1 corrigido

---
+++ exercicios/ex2/js/script.js
@@ -0,0 +1,7 @@
+function somar() {
+    let numero1 = document.querySelector('.num1');
+    let numero2 = document.querySelector('.num2');
+    let somaNum = num1 + num2
+}
+
+let botao = document.querySelector('.btnbtnSomarValor');
