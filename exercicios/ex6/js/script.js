41f2b7f4b1330cbc59b97fd7185b3f321130809e
s7lima <bruno.eslima@senacsp.edu.br>
2024-10-01 21:46:54 -0300

    Feat: Ex6 corrigido

--- exercicios/ex6/js/script.js
+++ exercicios/ex6/js/script.js
@@ -1,16 +1,15 @@
-function somar() {
-    let numero1 = document.querySelector('.num1').value;
-    let numero2 = document.querySelector('.num2').value;
-    
-    numero1 = parseInt(numero1);
-    numero2 = parseInt(numero2);
-
-    let soma = numero1 + numero2;
-
-    pResulado = document.querySelector("#resultado");
-
-    pResulado.innerHTML = `${numero1} + ${numero2} = ${soma}`;
+function exibirMaior() {
+    let num1 = parseFloat(document.querySelector("#num1").value);
+    let num2 = parseFloat(document.querySelector("#num2").value);
+    let num3 = parseFloat(document.querySelector("#num3").value);
+    let resultado = "";
+    if ((num1 >= num2) && (num1 >= num2))
+        resultado = `${num1} é maior!`
+    else if ((num2 >= num1) && (num2 >= num3))
+        resultado = `${num2} é maior!`
+    else
+        resultado = `${num3} é maior!`
+    document.querySelector("#resultado").innerHTML = resultado;
 }
-
-let botao = document.querySelector("button");
-botao.addEventListener('click', somar);
+ 
+document.querySelector(".btn").addEventListener('click', exibirMaior);
