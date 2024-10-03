92e13079b1826f28db82b9c513de18a5c2bb8656
s7lima <bruno.eslima@senacsp.edu.br>
2024-10-03 20:51:04 -0300

    Feat: Ex12 corrigido

--- exercicios/ex12/js/script.js
+++ exercicios/ex12/js/script.js
@@ -1,13 +1,33 @@
-function calcularresultado() {
-    let nome = document.querySelector('#nome').value;
-    let valorHora = parseFloat(document.querySelector('#valorhora').value);
-    let quantHora = parseFloat(document.querySelector('#quanthora').value);
+function calcular() {
+    let numero1 = parseFloat(document.querySelector('#num1').value);
+    let numero2 = parseFloat(document.querySelector('#num2').value);
+    let operador = document.querySelector("#calc").value;
 
-    let resultado = valorHora * quantHora;
-
-    document.querySelector("#resultado").innerHTML = 
-    `O valor recebido do trabalhador ${nome} é  R$ ${resultado.toFixed(2)}`;
-    
+    switch (operador) {
+        case "soma":
+            document.querySelector('h2').innerHTML = `A soma é ${numero1 + numero2}`;
+            break;
+        case "subtracao":
+            document.querySelector('h2').innerHTML = `A subtração é ${numero1 - numero2}`;
+            break;
+        case "multiplicacao":
+            document.querySelector('h2').innerHTML = `A multiplicacao é ${numero1 * numero2}`
+            break;
+        case "divisao":
+            if (numero2 == 0) {
+            document.querySelector('h2').innerHTML = `Não pode ser dividido por 0}`;
+            } else {
+                document.querySelector('h2').innerHTML = `A divisão é ${numero1 / numero2}`
+            }
+            break;
+        case "potenciacao":
+            document.querySelector('h2').innerHTML = `A potenciação é ${numero1 ** numero2}`
+            break;
+        case "radiciacao":
+            document.querySelector('h2').innerHTML = `A Raiz de ${numero1} é ${numero1 ** (1/numero2)}`
+            break;
+        
+    }
 }
-
-document.querySelector('.btn').addEventListener('click', calcularresultado);
+document.querySelector('button')
+        .addEventListener('click', calcular);
