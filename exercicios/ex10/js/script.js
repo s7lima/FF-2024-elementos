157aca98985aa7dbba0275ea5bd3029ddbc6debc
s7lima <bruno.eslima@senacsp.edu.br>
2024-10-02 21:44:36 -0300

    Feat: Ex10 feito

--- exercicios/ex10/js/script.js
+++ exercicios/ex10/js/script.js
@@ -1,13 +1,27 @@
-function calcularresultado() {
-    let nome = document.querySelector('#nome').value;
-    let valorHora = parseFloat(document.querySelector('#valorhora').value);
-    let quantHora = parseFloat(document.querySelector('#quanthora').value);
+function exibirMaior() {
+    let produto1 = document.querySelector("#prod1").value;
+    let produto2 = document.querySelector("#prod2").value;
+    let produto3 = document.querySelector("#prod3").value;
 
-    let resultado = valorHora * quantHora;
+    let valor1 = parseFloat(document.querySelector("#valor1").value);
+    let valor2 = parseFloat(document.querySelector("#valor2").value);
+    let valor3 = parseFloat(document.querySelector("#valor3").value);
 
-    document.querySelector("#resultado").innerHTML = 
-    `O valor recebido do trabalhador ${nome} é  R$ ${resultado.toFixed(2)}`;
-    
+    // Exibir apenas o menor
+    // resultado = "";
+    // let menor = Math.min(valor1, valor2, valor3); 
+    // resultado = `${menor}`
+    // document.querySelector("#resultado").innerHTML =resultado;
+
+    // Exibir resultado com nome!!!
+    let resultado = "";
+    if ((valor1 <= valor2) && (valor1 <= valor2))
+        resultado = ` ${produto1}, custa R$ ${valor1} é o menor valor!`
+    else if ((valor2 <= valor1) && (valor2 <= valor3))
+        resultado = `  ${produto2}, custa R$ ${valor2} é o menor valor!`
+    else
+        resultado = `${produto3}, custa R$ ${valor3} é menor valor!`
+    document.querySelector("#resultado").innerHTML = resultado;
 }
-
-document.querySelector('.btn').addEventListener('click', calcularresultado);
+ 
+document.querySelector(".btn").addEventListener('click', exibirMaior);
