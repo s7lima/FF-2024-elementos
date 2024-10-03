d8e272e47ef7a0480f194550d6894c57745bb277
s7lima <bruno.eslima@senacsp.edu.br>
2024-10-03 19:41:15 -0300

    Feat: Ex11 corrigido

--- exercicios/ex11/js/script.js
+++ exercicios/ex11/js/script.js
@@ -1,22 +1,24 @@
-function exibirTurno(params) {
-    let turno = document.querySelector("#turno").value;
-
-    resultado = ""
-    matutino = "Bom-Dia!"
-    vespertino = "Boa-Tarde!"
-    noturno = "Boa-Noite!"
-    invalido = "Valor Invalido!"
-
-    if ((turno === "m") || (turno === "M")){
-        resultado = ` ${matutino}`
-    } else if ((turno === "v") || (turno === "V")){
-        resultado = `  ${vespertino}`
-    } else if ((turno === "n") || (turno === "N")){
-        resultado = `  ${noturno}`
-    } else {
-        resultado = `${invalido}`
-    }               
-    document.querySelector("#resultado").innerHTML = resultado;
+/* Faça um algoritmo que pergunte em que turno o usuário estuda. 
+Peça para digitar M- matutino, V- Vespertino ou N- Noturno. 
+Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", 
+conforme o caso digitado. */
+function exibirCumprimento() {
+    let turno = document.querySelector('#turno').value;
+    switch (turno) {
+        case "M":
+            document.querySelector('section>h2').innerHTML = "Bom dia!";
+            break;
+        case "V":
+            document.querySelector('section>h2').innerHTML = "Boa Tarde!";
+            break;
+        case "N":
+            document.querySelector('section>h2').innerHTML = "Boa Noite!";
+            break;
+        default:
+            document.querySelector('section>h2').innerHTML = "Opção Inválida!";
+            break;
+    }
 }
-
-document.querySelector(".btn").addEventListener('click', exibirTurno);
+document
+    .querySelector('button')
+    .addEventListener('click', exibirCumprimento);
