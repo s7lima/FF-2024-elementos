f49394fa7337de57158ecd7669442eecb029f361
s7lima <bruno.eslima@senacsp.edu.br>
2024-10-08 19:20:35 -0300

    Fix: Ex16 acertado

--- exercicios/ex16/js/script.js
+++ exercicios/ex16/js/script.js
@@ -1,26 +1,44 @@
-function gerarTabuada () {
-    let numero = document.querySelector("#num").value;
+/*
+Desenvolva um gerador de tabuada, capaz de gerar através de qualquer número inteiro, 
+solicitando ao usuário o número e mostrando na tela conforme o exemplo abaixo:
+Tabuada de 5:
+5 x 1 = 5
+5 x 2 = 10
+...
+5 x 10 = 50 
+*/
+function gerarTabuada() {
+    let numero = document.querySelector('#numero').value;
 
-    let msgTabuada = ""
-    for (let i = 1; i <= 10; i++ ) {
-        msgTabuada += `${numero} X ${i} = ${numero * i} <br>`;
-        
-    }
-    document.querySelector('#resultado').innerHTML = msgTabuada;
-
-
-}
-
-function perguntar() {
-    let repetir = confirm("Deseja Gerar outra tabuada?");
-    if (repetir) {
-        document.querySelector('#resultado').innerHTML = ""; 
-        document.querySelector('#num').value = ""; 
+    if (isNaN(numero) == true || numero == "") {
+        alert("Número Inválido!");
+        document.querySelector('#numero').value = "";
     } else {
-        document.querySelector('#num').disabled = true; 
-        document.querySelector('button').disabled = true; 
+        let msgTabuada = "";
+        for (let i = 1; i <= 10; i++) {
+            msgTabuada += `${numero} X ${i} = ${numero * i} <br>`;
+        }
+        document.querySelector('h5').innerHTML = msgTabuada;
+        document.querySelector('#numero').disabled = true;
+        document.querySelector('.btn-primary').disabled = true;
+        document.querySelector('.btn-dark').disabled = false;
+    }
+
+
+}
+function liberarTabuada() {
+    let podeGerar = confirm("Deseja gerar outra tabuada?");
+
+    if (podeGerar == true) {
+        document.querySelector('#numero').disabled = false;
+        document.querySelector('.btn-primary').disabled = false;
+        document.querySelector('.btn-dark').disabled = true;
     }
 }
+document
+    .querySelector('.btn-primary')
+    .addEventListener('click', gerarTabuada);
 
-document.querySelector('button')
-.addEventListener('click', gerarTabuada)
+document
+    .querySelector('.btn-dark')
+    .addEventListener('click', liberarTabuada);
