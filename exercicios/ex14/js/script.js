17317dd45917cf7ea15cef258b64dc10128fb5a0
s7lima <bruno.eslima@senacsp.edu.br>
2024-10-09 19:19:32 -0300

    Feat : ex14 corrigido

--- exercicios/ex14/js/script.js
+++ exercicios/ex14/js/script.js
@@ -1,13 +1,60 @@
-function calcularresultado() {
-    let nome = document.querySelector('#nome').value;
-    let valorHora = parseFloat(document.querySelector('#valorhora').value);
-    let quantHora = parseFloat(document.querySelector('#quanthora').value);
+/*
+Faça um algoritmo que leia um número inteiro menor 
+que 1000 e imprima a quantidade de 
+centenas, dezenas e unidades do mesmo. 
+Observando os termos no plural a colocação do "e", 
+da vírgula entre outros.
+Exemplo: 326:  3 centenas, 2 dezenas e 6 unidades.
+*/
 
-    let resultado = valorHora * quantHora;
-
-    document.querySelector("#resultado").innerHTML = 
-    `O valor recebido do trabalhador ${nome} é  R$ ${resultado.toFixed(2)}`;
+function gerarResultado() {
+    let numero = parseInt(document.querySelector('#numero').value);
+    if (numero < 1 || numero >= 1000)
+        alert("Operação Inválida!");
     
+    else {
+        let centenas = parseInt(numero / 100);
+        let dezenas = parseInt((numero % 100) / 10);
+        let unidades = numero % 10;
+        let txtResultado = "";
+        //--------------CENTENAS ----------------
+        if (centenas > 1)
+            txtResultado = `${centenas} centenas`;
+        else if (centenas == 1)
+            txtResultado = `${centenas} centena`;
+        //--------------DEZENAS ----------------
+        if (dezenas > 1) {
+            if (txtResultado !== '') {
+                if (unidades == 0)
+                    txtResultado = `${txtResultado} e ${dezenas} dezenas.`
+                else
+                    txtResultado = `${txtResultado}, ${dezenas} dezenas`
+            } else
+                txtResultado = `${dezenas} dezenas`
+        } else if (dezenas == 1) {
+            if (txtResultado !== '') {
+                if (unidades == 0)
+                    txtResultado = `${txtResultado} e 1 dezena.`
+                else
+                    txtResultado = `${txtResultado}, 1 dezena`
+            } else
+                txtResultado = `${dezenas} dezena`
+        }
+        //-------------- UNIDADES ----------------
+        if (unidades > 1) {
+            if (txtResultado !== '')
+                txtResultado = `${txtResultado} e ${unidades} unidades.`
+            else
+                txtResultado = `${unidades} unidades`
+        } else if (unidades == 1) {
+            if (txtResultado !== '')
+                txtResultado = `${txtResultado} e 1 unidade.`
+            else
+                txtResultado = `1 unidade.`
+        }
+        document.querySelector('section>h2').innerHTML = txtResultado;
+    }
 }
-
-document.querySelector('.btn').addEventListener('click', calcularresultado);
+document
+    .querySelector('.btn-success')
+    .addEventListener('click', gerarResultado);
