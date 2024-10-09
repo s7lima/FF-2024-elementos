1e7d3487d3babc778395c2eb8c7c8586c4bb3eb7
s7lima <bruno.eslima@senacsp.edu.br>
2024-10-08 21:18:03 -0300

    Feat: ex17 feito e corrigido

--- exercicios/ex17/js/script.js
+++ exercicios/ex17/js/script.js
@@ -1,13 +1,25 @@
-function calcularresultado() {
-    let nome = document.querySelector('#nome').value;
-    let valorHora = parseFloat(document.querySelector('#valorhora').value);
-    let quantHora = parseFloat(document.querySelector('#quanthora').value);
+function gerarTabuada() {
+    let numero = parseFloat(document.querySelector('#numero').value);
 
-    let resultado = valorHora * quantHora;
+    if (isNaN(numero) || numero === "") {
+        alert("Número Inválido!");
+        document.querySelector('#numero').value = "";
+    } else {
+        let corpoTabela = document.querySelector('#corpo-tabela');
+        corpoTabela.innerHTML = ""; // Limpa a tabela antes de adicionar novos resultados
 
-    document.querySelector("#resultado").innerHTML = 
-    `O valor recebido do trabalhador ${nome} é  R$ ${resultado.toFixed(2)}`;
-    
+        for (let i = 1; i <= 50; i++) {
+            let unidade = i === 1 ? "pão" : "pães";
+            let linhaTabela = `<tr><td>${i} ${unidade}</td><td>R$ ${(numero * i).toFixed(2)}</td></tr>`;
+            corpoTabela.innerHTML += linhaTabela;
+        }
+
+        document.querySelector('#tabela-resultados').style.display = "table";
+        document.querySelector('#numero').disabled = true;
+        document.querySelector('.btn').disabled = true;
+    }
 }
 
-document.querySelector('.btn').addEventListener('click', calcularresultado);
+document
+    .querySelector('.btn')
+    .addEventListener('click', gerarTabuada);
